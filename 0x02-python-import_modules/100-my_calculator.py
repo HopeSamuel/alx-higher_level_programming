6ecb6c1e28e23f5ea4dfc8fa685648d1f2d71ba2
HopeSamuel <mariushope007>
2023-01-05 15:47:37 -0800

    6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,8 +10,10 @@ if len(sys.argv) - 1 != 3:
     sys.exit(1)
     ops = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator.Available operators: +, -, * and /") sys.exit(1)
+        print("Unknown operator.Available operators: +, -, * and /") 
 
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-            print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+        sys.exit(1)
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+
+        print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
