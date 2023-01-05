3fdf0e4f183cbd24186bf7d62b2366d07affc1d8
HopeSamuel <mariushope007>
2023-01-05 13:15:33 -0800

    3rd task

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     total = 0
     for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
-        print("{}".format(total))
+    print("{}".format(total))
