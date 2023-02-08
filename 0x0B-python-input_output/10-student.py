6fce6ce86cd4d7bd8cb16704258015b53db5cfb7
HopeSamuel <mariushope007>
2023-02-07 22:48:40 -0800

    why

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+This repository is fot python input and output 
