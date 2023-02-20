f6ab6da64cc3b29a9fe5c49fa54271cfe8bba9e5
HopeSamuel <113640033+HopeSamuel@users.noreply.github.com>
2023-02-20 08:15:04 +0100

    Try

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,6 @@
+
 #!/usr/bin/python3
-
-    def simple_delete(my_dict,
-        if key in my_dict:
-            del my_dict[key]
-            return my_dict
-
+def simple_delete(my_dict, key=""):
+    if key in my_dict:
+        del my_dict[key]
+    return my_dict
