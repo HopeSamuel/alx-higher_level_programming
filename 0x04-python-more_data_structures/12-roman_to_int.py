d1ed88e1d5c7d0205727be1f8bd023ac67cf3dab
HopeSamuel <113640033+HopeSamuel@users.noreply.github.com>
2023-02-20 08:06:59 +0100

    Try

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,25 +1,24 @@
-
-#!/usr/bin/python3
-def to_subtract(list_num):
-    to_sub = 0
-    max_list = max(list_num)
-
-    for n in list_num:
-        if max_list > n:
-            to_sub += n
-
-            return (max_list - to_sub)
-        def roman_to_int(roman_string):
-
-            if not roman_string:
-                return 0
-         if not isistance(:
-                 return 0
-
-                 rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-                 list_keys = list 67num = 0    l                                                                       last_rom = 0 list_num = [0]z
                                                                                                 for ch in roman_string:
-                                                                                                            for r_num in list_keys:
+        #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    decs = [roman_dict[x] for x in roman_string]
+    output = 0
+    for i in range(len(decs)):
+        output += decs[i]
+        if decs[i - 1] < decs[i] and i != 0:
+            output -= (decs[i - 1] + decs[i - 1])
+    return output                                                                                                    for r_num in list_keys:
                                                                                                                             if r_num == ch:
                                                                                                                                                 if rom_n.get(ch) <= last_rom:
                                                                                                                                                                         num += to_subtract(list_num)
