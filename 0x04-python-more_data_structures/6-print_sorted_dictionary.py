7c418dbccdffa9e19230e0807225dcc751f3cbbc
HopeSamuel <113640033+HopeSamuel@users.noreply.github.com>
2023-02-20 08:12:40 +0100

    Try

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,5 @@
+
 #!/usr/bin/python3
-
-
-def print_sorted_dictionary (my_dict):
+def print_sorted_dictionary(my_dict):
     for key in sorted(my_dict.keys()):
         print("{:s}: {}".format(key, my_dict[key]))
-
