88239a45e860d3dbc7aea5ef15bfc1bbd07c2e9c
HopeSamuel <113640033+HopeSamuel@users.noreply.github.com>
2023-02-20 08:55:15 +0100

    Mo

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,6 +3,7 @@
 Defines a class Rectangle
 """
 
+
 class Rectangle:
     """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
@@ -23,24 +24,27 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         """getter for the private instance attribute height"""
         return self.__height
+
     @height.setter
     def height(self, value):
-
         """setter for the private instance attribute height"""
-        if type(value) is not intraise TypeError("height must be an integer")
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     def area(self):
         """returns the area of the rectangle"""
         return self.__width * self.__height
+
     def perimeter(self):
         """returns the perimeter of the rectangle"""
-
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
