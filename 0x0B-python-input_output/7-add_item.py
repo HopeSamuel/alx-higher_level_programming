2a3e96f48c157e2ccac8f6f159b3aeacbd8f100a
HopeSamuel <113640033+HopeSamuel@users.noreply.github.com>
2023-05-15 09:22:09 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""
+"""Add all arguments to a Python list and save them to a file."""
 
-Script that adds all arguments to a Python list, and then saves them to a file
-"""
 
-from sys import argv
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+import sys
 
-filename = "add_item.json"
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    json_list = load_from_json_file(filename)
-except FileNotFoundError:
-    json_list = []
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
 
-for arg in argv[1:]:
-    json_list.append(arg)
 
-save_to_json_file(json_list, filename)
+
+
+
+
+
+
 
 
