70b83ad2d7cf106c88e884c4123880d861e16a73
HopeSamuel <113640033+HopeSamuel@users.noreply.github.com>
2023-05-15 09:12:40 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""
-Contains the read_file function
-"""
+"""Defines a text file-reading function."""
+
 
 def read_file(filename=""):
-    """""reads a text file(UTF8) and prints it to stdout"""
-    with open(filename, "r", encoding="utf-8") as f:
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
