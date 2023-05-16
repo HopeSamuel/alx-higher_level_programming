555dd2d9e1d30c75ac45c6677cbd645f63a20182
HopeSamuel <113640033+HopeSamuel@users.noreply.github.com>
2023-05-16 11:17:33 +0100

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,12 +4,20 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
+
 myObject.incr = function () {
   this.value++;
 };
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
+
+
+
+
+
+
